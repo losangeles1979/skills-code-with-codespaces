0169f9d62d48feb313b3148b08fce95c5f205f76
losangeles1979 <ROCKETDYNE1979@GMAIL.COM>
2026-03-16 00:50:50 +0000

    add the train

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -4,3 +4,12 @@ sudo apt-get update
 sudo apt-get install sl
 echo "export PATH=\$PATH:/usr/games" >> ~/.bashrc
 echo "export PATH=\$PATH:/usr/games" >> ~/.zshrc
+echo "Choo choo! Running the locomotive..."
+echo "      ====        ________                "
+echo "  _D _|  |_______/        \\__I_I_____===__| "
+echo "   |(_)---  |   H\\________/ |   |        =| "
+echo "   /     |  |   H  |  |     |   |         | "
+echo "  |      |  |   H  |__--------------------| "
+echo "  | ________|___H__/__|_____/[][]~\\_______| "
+echo "  |/ |   |-----------I_____I [][] []  D   | "
+echo "____|_____|_________I_____I [][] []  D   | "
